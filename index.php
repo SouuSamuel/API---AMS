810c364a144be137988f3c83a5b3e5510f2b4909
Samuel <samueljgn08@gmail.com>
2025-08-11 17:07:18 -0300

    Criação do banco de dados

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Iniciada</title>
+</head>
+<body>
+<h1>Revisão Javascript</h1>
+<script>
+const nome = "Fernando"     
+var numero = 11
+let idade = 35
+
+let meninas = [
+    "Luana",
+    "Maryelen",
+    "Maria Clara",
+    "Marcelo"
+]
+
+for(let i = 0; i < meninas.length; i++)
+{
+ document.write(meninas[i]+"<br/>")
+}
+
+
+let marcelo = {
+  "cabelo" : "2 metros "
+  "beleza" : "bonitinho",
+  "Nome" : "Marcelinho",
+  "Idade" : "17",
+  "Peso" : "1.61"
+
+}
+let pedro = {
+    "Nome":"Pedro",
+    "Idade":"31",
+    "Peso":"70",
+    "Altura":"1.76"
+}
+let yago = {
+"Nome":"Yago",
+"Idade":"17",
+"Peso":"65",
+"Altura":"1.64"
+}
+
+let meninos = [marcel, yago, pedro]
+for(let i = 0; i < meninos.length; i++)
+{
+    document.write(meninos[i].Nome+"<br>")
+}
+
+
+</script>
+</body>
+</html>
